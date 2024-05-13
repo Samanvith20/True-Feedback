e8c1e2db42edee3206ea68defa06955c74055f8f
Samanvith <Samanvith2005@gmail.com>
2024-05-13 11:10:28 +0530

    resend email

---
+++ src/helpers/EmailVerification.ts
@@ -0,0 +1,18 @@
+import {resend} from "@lib/resendEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import  {VerificationEmail} from "@/helpers/verifyEmail";
+
+export  const sendVerificationEmail = async (email: string, username: string, otp: string): Promise<ApiResponse> => {
+   try {
+    await resend.emails.send({
+      from: 'yervala@gmail.com',
+      to: email,
+      subject: 'Mystery Message Verification Code',
+      react: VerificationEmail({ username, otp: otp }),
+    })
+    return { success: true, message: 'Verification email sent successfully.' };
+   } catch (emailError) {
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
+   }
+}
